e0d652cba8493ed8fa7f4dcc33c707fc5dab75e7
radamesk9 <64846148+radamesk9@users.noreply.github.com>
2020-07-15 09:34:38 -0300

    Add files via upload
    Atualização de arquivos css na branch master. 15.07.2020

--- css2204/JQUERY2204.js
+++ css2204/JQUERY2204.js
@@ -37,8 +37,16 @@ function scrollFunction() {
     }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+// When the user clicks the button, scroll to the top of the document
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
+//when user clicks the button 'lightmode' the site will be lit.
+ $(document).ready(function() {
+  $('.day-night').click(function() {
+    $('body').toggleClass('night');
+  });
+});
